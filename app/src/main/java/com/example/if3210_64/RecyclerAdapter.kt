d07eb96f2924437e8346d96383b16c57e4c71e28
Allief Nuriman <57051116+alliefn@users.noreply.github.com>
2022-02-28 23:57:56 +0700

    Sudah bisa fetch

--- app/src/main/java/com/example/if3210_64/RecyclerAdapter.kt
+++ app/src/main/java/com/example/if3210_64/RecyclerAdapter.kt
@@ -6,18 +6,17 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import org.w3c.dom.Text
 
 class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
+    private var items: ArrayList<News> = ArrayList()
 
-
-    private var title = arrayOf("Chapter One", "Chapter Two", "Chapter Three", "Chapter Four", "Chapter Five", "Chapter Six", "Chapter Seven", "Chapter Eight")
-    private var details = arrayOf("Item one details", "Item two details", "Item three details", "Item five details",
-        "Item six details", "Item seven details", "Item eight details")
+    private var title = arrayOf("Grader Build", "Chapter Two", "Chapter Three", "Chapter Four", "Chapter Five", "Chapter Six", "Chapter Seven", "Chapter Eight")
+    private var details = arrayOf("Item one details", "Item two details", "Item three details", "Item four details","Item five details",
+        "Item six details", "Item seven details", "Item eight details", "Item nine details", "Item ten details")
     private var images = intArrayOf(R.drawable.ic_android_black_24dp, R.drawable.ic_android_black_24dp, R.drawable.ic_android_black_24dp,
         R.drawable.ic_android_black_24dp, R.drawable.ic_android_black_24dp, R.drawable.ic_android_black_24dp,
-        R.drawable.ic_android_black_24dp, R.drawable.ic_android_black_24dp)
+        R.drawable.ic_android_black_24dp, R.drawable.ic_android_black_24dp, R.drawable.ic_android_black_24dp, R.drawable.ic_android_black_24dp)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.card_layout,parent,false)
@@ -25,13 +24,22 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        // TO DO: ATUR PENEMPATAN variabel items
         holder.itemTitle.text = title[position]
-        holder.itemDetail.text = title[position]
+        //holder.itemDetail.text = details[position]
+        holder.itemDetail.text = items.toString()
         holder.itemImage.setImageResource(images[position])
     }
 
     override fun getItemCount(): Int {
         return title.size
+    }
+
+    fun updateNews(updatedNews: ArrayList<News>) {
+        items.clear()
+        items.addAll(updatedNews)
+
+        notifyDataSetChanged()
     }
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
