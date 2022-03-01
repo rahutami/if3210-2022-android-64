bd12720c88aa5a12fbea8d2a6630a8bec3215128
Allief Nuriman <57051116+alliefn@users.noreply.github.com>
2022-03-01 17:54:45 +0700

    Bottom navbar placeholder content, also preparing other fragment to integrate later

--- app/src/main/java/com/example/if3210_64/fragments/NewsArticleFragment.kt
+++ app/src/main/java/com/example/if3210_64/fragments/NewsArticleFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [News.newInstance] factory method to
+ * Use the [NewsArticleFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class News : Fragment() {
+class NewsArticleFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class News : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false)
+        return inflater.inflate(R.layout.fragment_news_article, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class News : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment News.
+         * @return A new instance of fragment NewsArticleFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            News().apply {
+            NewsArticleFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
